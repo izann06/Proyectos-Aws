a16ee814c0704bccd97ff0570611917301783d65
Izan Marcos Martinez <izan.mireya.consu@gmail.com>
2026-09-12 08:18:18 +0200

    añadido terraform outputs para mostrar la IP del servidor de AWS por SSH

---
+++ Servidor-Web-y-Automatizado-con-GitHub-Actions/terraform/outputs.tf
@@ -0,0 +1,9 @@
+output "ip_publica_del_servidor" {
+  description = "Esta es la IP pública donde se puede acceder a nuestra API"
+  value       = aws_instance.mi_servidor.public_ip
+}
+
+output "comando_ssh" {
+  description = "Comando para conectarse al servidor"
+  value       = "ssh -i $HOME/.ssh/aws_ubuntu_key ubuntu@${aws_instance.mi_servidor.public_ip}"
+}
